e6d1b21d05aa4166b83a4ce392a507762f819a80
HungNT <hung.nt@urbox.vn>
2022-12-23 08:45:29 +0700

    updated code

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -5,9 +5,14 @@ import { Orders } from './entity/orders.entity';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { OrderProducts } from './entity/order-products.entity';
+import { CartModule } from 'src/cart/cart.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Orders, OrderProducts]), ProductModule],
+  imports: [
+    TypeOrmModule.forFeature([Orders, OrderProducts]),
+    ProductModule,
+    CartModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
